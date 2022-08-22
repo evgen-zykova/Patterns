44470f71702cf8cf3f464e5a3d1cbec1f5975439
Евгения Пискарева <evgen-zykova@mail.ru>
2022-08-23 00:13:09 +0300

    Change DataGenerator

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -1,7 +1,6 @@
 package ru.netology.delivery.data;
 
 import com.github.javafaker.Faker;
-import lombok.Value;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -11,42 +10,23 @@ public class DataGenerator {
     private DataGenerator() {
     }
 
-    public static class Redistration {
-        private Redistration() {
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            Faker faker = new Faker(new Locale("ru"));
+            return new UserInfo(
+                    faker.address().cityName(),
+                    faker.name().firstName() + " " + faker.name().lastName(),
+                    faker.phoneNumber().phoneNumber());
+        }
+
+        public static String generateDate(int days) {
+            return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         }
     }
-
-    public static String generateDate(int days) {
-        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    }
-
-    public static String generateCity() {
-        String[] cities = new String[]{"Москва", "Пенза", "Волгоград", "Саратов", "Казань"};
-        int itemIndex = (int) (Math.random() * cities.length);
-        return cities[itemIndex];
-    }
-
-    public static String generateName() {
-        Faker faker = new Faker(new Locale("ru"));
-        String name = faker.name().fullName();
-        return name;
-    }
-
-    public static String generatePhone() {
-        Faker faker = new Faker(new Locale("ru"));
-        return faker.phoneNumber().phoneNumber();
-    }
-
-    public static UserInfo generateUser() {
-        UserInfo user = new UserInfo(generateCity(), generateName(), generatePhone());
-        return user;
-    }
-
-    @Value
-    public static class UserInfo {
-        String city;
-        String name;
-        String phone;
-    }
 }
+
+
 
--- src/test/java/ru/netology/delivery/test/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/delivery/test/CardDeliveryOrderTest.java
@@ -1,22 +1,18 @@
 package ru.netology.delivery.test;
 
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
+import ru.netology.delivery.data.UserInfo;
 
-import java.time.Duration;
-
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-import static ru.netology.delivery.data.DataGenerator.generateDate;
-import static ru.netology.delivery.data.DataGenerator.generateUser;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+import static ru.netology.delivery.data.DataGenerator.Registration.generateDate;
 
 
 public class CardDeliveryOrderTest {
+    UserInfo user = DataGenerator.Registration.generateUser("ru");
 
     @BeforeEach
     void setUp() {
@@ -26,12 +22,11 @@ public class CardDeliveryOrderTest {
 
     @Test
     void shouldSuccessfulPlanAndReplanMeeting() {
-        DataGenerator.UserInfo user = generateUser();
         $("[data-test-id=city] input").setValue(user.getCity());
         $(".calendar-input input").doubleClick().sendKeys(Keys.BACK_SPACE);
         $(".calendar-input input").setValue(generateDate(6));
         $("[data-test-id=name] input").setValue(user.getName());
-        $("[data-test-id=phone] input").setValue(user.getPhone());
+        $("[data-test-id=phone] input").setValue(user.getNumber());
         $("[data-test-id=agreement]").click();
         $(".button").click();
         $("[data-test-id=success-notification]")
@@ -39,9 +34,10 @@ public class CardDeliveryOrderTest {
         $(".calendar-input input").doubleClick().sendKeys(Keys.BACK_SPACE);
         $(".calendar-input input").setValue(generateDate(7));
         $(".button").click();
-        $(withText("Необходимо подтверждение")).shouldBe(Condition.visible, Duration.ofSeconds(150));
-        $(".button__text").click();
-        $("[data-test-id=success-notification]").shouldBe(Condition.visible, Duration.ofSeconds(150)).shouldHave(exactText("Успешно! Встреча успешно запланирована на " + generateDate(7)));
+        $$(".button__text").find(exactText("Перепланировать")).click();
+        $(".notification__content")
+                .shouldHave(text("Встреча успешно запланирована на " + generateDate(7)))
+                .shouldBe(visible);
     }
 }
 
